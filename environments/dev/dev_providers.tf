879a72cc0bd59733d806a0eb7a971c6734df8a5c
suatceyhan <suatceyhan@gmail.com>
2023-12-13 10:11:40 -0500

    test51

--- environments/dev/dev_providers.tf
+++ environments/dev/dev_providers.tf
@@ -19,8 +19,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  client_id       = var.AZURE_CLIENT_ID
-  client_secret   = var.AZURE_CLIENT_SECRET
-  tenant_id       = var.AZURE_TENANT_ID
-  subscription_id = var.AZURE_SUBSCRIPTION_ID
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
+  subscription_id = var.ARM_SUBSCRIPTION_ID
 }
--- environments/dev/dev_variables.tf
+++ environments/dev/dev_variables.tf
@@ -682,7 +682,7 @@ variable "req_rule_back_http" {
   type        = string
 }
 
-variable "AZURE_CLIENT_ID" {}
-variable "AZURE_CLIENT_SECRET" {}
-variable "AZURE_TENANT_ID" {}
-variable "AZURE_SUBSCRIPTION_ID" {}
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+variable "ARM_TENANT_ID" {}
+variable "ARM_SUBSCRIPTION_ID" {}
