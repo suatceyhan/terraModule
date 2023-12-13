0d3f2b5fdc60f58366b721e43f57231929312dd1
suatceyhan <suatceyhan@gmail.com>
2023-12-13 10:36:48 -0500

    test54

--- scripts/plan.sh
+++ scripts/plan.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 cd environments/dev
-terraform plan -var-file="dev.tfvars"
+#terraform plan -var-file="dev.tfvars"
+
+terraform plan \
+    -var="ARM_CLIENT_ID=${secrets.ARM_CLIENT_ID}" \
+    -var="ARM_CLIENT_SECRET=${secrets.ARM_CLIENT_SECRET}" \
+    -var="ARM_TENANT_ID=${secrets.ARM_TENANT_ID}" \
+    -var="ARM_SUBSCRIPTION_ID=${secrets.ARM_SUBSCRIPTION_ID}" \
+    -var-file="dev.tfvars"
+
